5d5dfbb82a06ed5bcf89a7f1d4c9d55150b14bb7
iainv <iainvisa@gmail.com>
2025-04-28 22:55:16 +0200

    renamed main.py

--- main-old.py
+++ main-old.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from utilities import *
+
 import flask
 from flask import Flask, send_file, render_template, redirect
 import requests
@@ -12,73 +12,35 @@ prods_path = base_url + "projects/panson/databases/productes/documents/productes
 
 
 
-class Localization():
-    def __init__(self, lan):
-        self.lan = lan
-        self.loc_json = json.loads(open("localization.json").read())
-        self.all_langs = (lang for lang in self.loc_json.keys() if lang != "colors")
-        self.colors = self.loc_json["colors"]
-        self.loc = self.loc_json[lan]
-
-    def __getattr__(self, item):
-        try:
-            return self.loc[item.replace("_", "-")]
-        except KeyError:
-            return self.loc_json["cat"][item.replace("_", "-")]
-
-    def __getitem__(self, item):
-        return self.loc[item]
-
-    @staticmethod
-    def upper(string ):
-        return string.upper()
-
-    def update(self, lan):
-        if self.lan != lan:
-            self.__init__(lan)
-            return self
-        else:
-            return self
-
-global loc
-loc = Localization("cat")
 
 
-def carregar_totes_collecions(loc):
-    sprint("Carregant collecions")
-    try:
-        all_cols = json.loads(requests.get(cols_path).text)["documents"]
-    except:
-        return "Could not read collection data at: <br> "+ cols_path
-    html = ""
-    all_data = []
+def get_col_data(path):
+    url = base_url + path
+    print("Getting collection data from url: ")
+    print(url)
+    data = json.loads(requests.get(url).text)
+    description = ""
+    name = ""
+    id = url.split("/")[-1]
+    if "fields" in data:
+        print(data["fields"])
+        if "nom" in data["fields"]:
+            name = data["fields"]["nom"]["stringValue"]
+        if "descripcio" in data["fields"]:
+            description = data["fields"]["descripcio"]["stringValue"]
+    return id, name, description
+
+
+def get_collections(loc):
+    print("Getting all collections:")
+    all_cols = json.loads(requests.get(cols_path).text)["documents"]
+    data=[]
     for col in all_cols:
         col_path = col["name"]
-        url = base_url + col_path
-        print1("URL:", url)
-        data = json.loads(requests.get(url).text)
-        descripcio = ""
-        nom = ""
-        id = url.split("/")[-1]
-        if "fields" in data:
-            if "nom-" + loc.lan in data["fields"]:
-                nom = read_data_type(data["fields"]["nom-" + loc.lan])
-            elif "nom" in data["fields"]:
-                nom = read_data_type(data["fields"]["nom"])
-            if "descripcio" + loc.lan in data["fields"]:
-                descripcio = read_data_type(["fields"]["descripcio" + loc.lan])
-            elif "descripcio" in data["fields"]:
-                descripcio = read_data_type(["fields"]["descripcio"])
-        all_data.append((id, nom, descripcio))
-    html += render_template("collecions.html", data=all_data, loc=loc)
-    html += render_template("navigation.html", loc = loc)
+        data.append(get_col_data(col_path))
+    html = render_template("collecions.html", data=data, loc=loc)
+    #print(html)
     return html
-
-
-
-def carregar_galeria():
-    pass
-
 
 def get_product_data(path):
     url = base_url + path
@@ -194,12 +156,34 @@ def get_products_by_attribute(attributes = [], values = [], origin = "/", templa
 
 
 
+class Localization():
+    def __init__(self, lan):
+        self.loc_json = json.loads(open("localization.json").read())
+        self.all_langs = (lang for lang in self.loc_json.keys() if lang != "colors")
+        self.colors = self.loc_json["colors"]
+        self.loc = self.loc_json[lan]
+
+    def __getattr__(self, item):
+        try:
+            return self.loc[item.replace("_", "-")]
+        except KeyError:
+            return self.loc_json["cat"][item.replace("_", "-")]
+
+    def __getitem__(self, item):
+        return self.loc[item]
+
+
+
+    @staticmethod
+    def upper(string ):
+        return string.upper()
+
+
 
 
 
 @app.route("/")
 def redirect_to_cat():
-    loc.update("cat")
     return redirect("/cat/")
 
 @app.route("/static/<path:path>", defaults={"lan": "cat"})
@@ -210,19 +194,25 @@ def get_static(lan, path):
 
 @app.route("/<lan>/")
 def index(lan):
-    loc.update(lan)
+    loc = Localization(lan)
     return render_template('index.html', loc = loc) + render_template("navigation.html", origin="hide", loc = loc)
 
 
 
+@app.route("/<lan>/navigation")
+def navigation(lan):
+    loc = Localization(lan)
+    return render_template("navigation.html", loc=loc)
+
 @app.route("/<lan>/collecions")
 def collections(lan):
-    loc.update(lan)
-    return carregar_totes_collecions(loc)
+    loc = Localization(lan)
+    html = get_collections(loc=loc)
+    return html + render_template("navigation.html", loc = loc)
 
 @app.route("/<lan>/collecions/<col>")
 def productes_per_col(lan, col):
-    loc.update(lan)
+    loc = Localization(lan)
     html = get_products_by_attribute("collecio", col, template="galeria.html", titol=col.capitalize(), subtitol=loc.gal_collecio, loc=loc)
     if html:
         return html + render_template("navigation.html", origin = None, loc = loc)
@@ -230,7 +220,7 @@ def productes_per_col(lan, col):
 
 @app.route("/<lan>/peces_uniques")
 def peces_uniques(lan):
-    loc.update(lan)
+    loc = Localization(lan)
     html = get_products_by_attribute("unica", True, loc = loc )
     if html:
         return html + render_template("navigation.html",  origin = None, loc = loc)
@@ -239,7 +229,7 @@ def peces_uniques(lan):
 
 @app.route("/<lan>/productes/<id>/<material>/<variacio>")
 def mostrar_peca_material_variacio(lan, id, material, variacio):
-    loc.update(lan)
+    loc = Localization(lan)
     print("VARIACIO:", variacio)
     html = get_products_by_attribute("id", id, first_only=True, loc = loc, material = material, variacio = variacio)
     if html:
@@ -248,7 +238,7 @@ def mostrar_peca_material_variacio(lan, id, material, variacio):
 
 @app.route("/<lan>/productes/<id>/<material>")
 def mostrar_peca_material(lan, id, material):
-    loc.update(lan)
+    loc = Localization(lan)
     html = get_products_by_attribute("id", id, first_only=True, loc = loc, material = material)
     if html:
         return html + render_template("navigation.html", origin = None, loc = loc)
@@ -256,7 +246,7 @@ def mostrar_peca_material(lan, id, material):
 
 @app.route("/<lan>/productes/<id>")
 def mostrar_peca(lan, id):
-    loc.update(lan)
+    loc = Localization(lan)
     html = get_products_by_attribute("id", id, first_only=True, loc = loc)
     if html:
         return html + render_template("navigation.html", origin = None, loc = loc)
@@ -264,7 +254,7 @@ def mostrar_peca(lan, id):
 
 @app.route("/<lan>/productes")
 def mostrar_tot(lan):
-    loc.update(lan)
+    loc = Localization(lan)
     html = get_products_by_attribute(template="galeria.html", titol=loc.gal_totes, subtitol="PANSON", loc=loc)
     if html:
         return html + render_template("navigation.html", origin = None, loc = loc)
@@ -278,7 +268,7 @@ def mostrar_tot(lan):
 
 @app.route("/<lan>/contacte")
 def contatce(lan):
-    loc.update(lan)
+    loc = Localization(lan)
     html = render_template("contacte.html", loc=loc)
     return html + render_template("navigation.html", origin = None, loc = loc)
 
