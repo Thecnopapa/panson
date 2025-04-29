ce10d56b47622a82b5f72d963ea0d25aecdbc0e4
iainv <iainvisa@gmail.com>
2025-04-30 01:58:28 +0200

    Shopping cart button Firestore/firebase setup
    (cherry picked from commit 7635e26c67db0d4ccf039c8d92a8659c0e6b393b)

---
+++ static/scripts/firebase.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCiEJyCJSC9AdViZP2PGmBlr8aibCC8f8",
+  authDomain: "panson.firebaseapp.com",
+  projectId: "panson",
+  storageBucket: "panson.firebasestorage.app",
+  messagingSenderId: "746452924859",
+  appId: "1:746452924859:web:75905f36a5bfaadca8ec9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
