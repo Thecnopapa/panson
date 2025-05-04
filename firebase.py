9b285e10d128e9309343cee5898907c37bc003cb
iainv <iainvisa@gmail.com>
2025-05-04 16:52:49 +0200

    moved credentials out of the secure folder

--- firebase.py
+++ firebase.py
@@ -1,11 +1,17 @@
 from utilities import *
 from flask import request
+import os, sys
 try:
     import firebase_admin
     from firebase_admin import credentials, firestore
+    print(os.getcwd())
+    print(os.listdir(os.getcwd()))
+    print("/")
+    print(os.listdir("/"))
+    print("./")
+    print(os.listdir("./"))
 
-
-    cred = credentials.Certificate("./secure/panson-791122483313.json")
+    cred = credentials.Certificate(".secure-panson-791122483313.json")
     app = firebase_admin.initialize_app(cred)
     db = firestore.client(app, database_id="productes")
 
