123736a69e27b754245373e2986855687834d6c4
iainv <iainvisa@gmail.com>
2025-05-05 01:23:58 +0200

    Fixed credentials

--- firebase.py
+++ firebase.py
@@ -4,8 +4,12 @@ import os, sys
 try:
     import firebase_admin
     from firebase_admin import credentials, firestore
-    cred = credentials.Certificate(os.environ.get('FIREBASE_CREDENTIALS'))
-    app = firebase_admin.initialize_app(cred)
+    if "FIREBASE_CREDENTIALS" in os.environ:
+        cred = credentials.Certificate(os.environ.get('FIREBASE_CREDENTIALS'))
+        app = firebase_admin.initialize_app(cred)
+    else:
+        app = firebase_admin.initialize_app()
+
     db = firestore.client(app, database_id="productes")
     productes = db.collection("productes")
 
--- firestore.py
+++ firestore.py
@@ -3,33 +3,58 @@ from utilities import *
 from google.oauth2 import service_account
 import json
 
+try:
+    '''SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin']
+    SERVICE_ACCOUNT_FILE = '/path/to/service.json'
+
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
-credentials = service_account.Credentials.from_service_account_file(
-    os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
-storage_client = storage.Client(credentials=credentials, project="panson")
-bucket = storage_client.bucket("panson.firebasestorage.app")
+    print("Service account json")
+    print(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+    #try:
+    service_account_info = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    print("Service account info")
+    print(service_account_info)
+
+    credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+    service_account.Credentials()
+    #except:
+        #credentials = service_account.Credentials.from_service_account_file(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+
+    print(credentials)'''
+    try:
+        credentials = service_account.Credentials.from_service_account_file(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+        storage_client = storage.Client(credentials=credentials, project="panson")
+    except:
+        storage_client = storage.Client(project="panson")
+    bucket = storage_client.bucket("panson.firebasestorage.app")
 
 
-sprint("Firestore initialized")
-
+    sprint("Firestore initialized")
+except Exception as e:
+    sprint("Error importing Firestore:")
+    print(e)
 
 
 def list_blobs(prefix = None ):
-    sprint("Lists all the blobs in the bucket")
-    bucket_name = "panson.firebasestorage.app"
+    try:
+        sprint("Lists all the blobs in the bucket")
 
-    print(storage_client)
+        print(storage_client)
 
-    # Note: Client.list_blobs requires at least package version 1.17.0.
+        # Note: Client.list_blobs requires at least package version 1.17.0.
 
-    print(bucket.__dict__)
-    blobs = bucket.list_blobs(prefix=prefix)
-    print(blobs.__dict__)
+        print(bucket.__dict__)
+        blobs = bucket.list_blobs(prefix=prefix)
+        print(blobs.__dict__)
 
-    # Note: The call returns a response only when the iterator is consumed.
-    blob_list = []
-    for blob in blobs:
-        blob_list.append(blob.name)
+        # Note: The call returns a response only when the iterator is consumed.
+        blob_list = []
+        for blob in blobs:
+            blob_list.append(blob.name)
+        return blob_list
+    except:
+       return []
 
-    return blob_list
