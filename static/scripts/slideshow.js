2e0388f14da18a9432626293888fc2a9b42f17a7
iainv <iainvisa@gmail.com>
2025-05-05 02:08:02 +0200

    Slideshow tweaks

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -5,10 +5,18 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("slide-content");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.translate = "-100%";
+    slides[i].style.zIndex = -10;
+
+
+
   }
   slideIndex++;
+  slides[slideIndex].style.translate = "100%";
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.zIndex = -1;
+  slides[slideIndex-1].style.translate = "0";
+
+
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
