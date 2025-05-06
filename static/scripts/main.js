458e9a09983361cba1869a420c515661840d9eb2
iainv <iainvisa@gmail.com>
2025-05-06 23:37:31 +0200

    Title sliding in index

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,4 @@
 
-
-
 function main() {
   console.log("Hello, World!");
 
@@ -9,3 +7,36 @@ function main() {
 
 main();
 
+let previousHeight = $(window).scrollTop()
+
+$(window).scroll(function () {
+  const title = document.getElementById('title');
+
+  console.log($(window).scrollTop() ,$(title).offset().top, $(window).scrollTop() >= $(title).offset().top );
+  if (title.style.position == "absolute" && ($(window).scrollTop() >= previousHeight)) {
+      if ($(window).scrollTop() >= $(title).offset().top ) {
+        console.log($(title).offset().top, previousHeight);
+        previousHeight = $(window).scrollTop()
+        document.getElementById('title').style.transition = "height 0.3s ease";
+        document.getElementById('title').style.position = "fixed";
+        document.getElementById('title').style.top = "0";
+        document.getElementById('title').style.height = "7dvh";
+        document.getElementById('title').style.paddingBottom = "0";
+        document.getElementById('title').style.paddingTop = "0";
+      }
+  }
+  else if (title.style.position == "fixed" && ($(window).scrollTop() < previousHeight)){
+      if ($(window).scrollTop() <= window.innerHeight / 2 ) {
+        console.log($(title).offset().top, previousHeight);
+        previousHeight = $(window).scrollTop()
+        document.getElementById('title').style.transition = "0.3s ease";
+        document.getElementById('title').style.height = "20dvh";
+
+        document.getElementById('title').style.top = "30dvh";
+
+        document.getElementById('title').style.paddingBottom = "5dvh";
+        document.getElementById('title').style.paddingTop = "5dvh";
+        document.getElementById('title').style.position = "absolute";
+      }
+    }
+});
