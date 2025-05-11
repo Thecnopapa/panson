4c7b959877846727135ff4e03e8734528f5b2304
iainv <iainvisa@gmail.com>
2025-05-11 16:56:38 +0200

    fixed title

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -7,7 +7,6 @@ function main() {
 
 main();
 let windowHeight = window.innerHeight
-let previousHeight = $(window).scrollTop()
 const maxScroll = windowHeight * 0.35
 const onClickLink = document.getElementById('title').onclick
 console.log(onClickLink)
@@ -28,42 +27,9 @@ $(window).scroll(function () {
   console.log(scroll, scroll/maxScroll);
   title.style.top = String(35-(35*scroll/maxScroll)).concat("dvh");
   title.style.height = String(20-(13*scroll/maxScroll)).concat("dvh");
-  title.style.left = String(40 - (20*scroll/maxScroll)).concat("dvw");
+  title.style.left = String(20 + (20*scroll/maxScroll)).concat("dvw");
   title.style.width = String(60 - (40*scroll/maxScroll)).concat("dvw");
   title.style.paddingBottom = String(5 - (5*scroll/maxScroll)).concat("dvh");
   title.style.paddingTop = String(5 - (5*scroll/maxScroll)).concat("dvh");
 
-
-  if (title.style.position == "absolute" && ($(window).scrollTop() >= previousHeight ) && false) {
-      if ($(window).scrollTop() >= $(title).offset().top) {
-        console.log($(title).offset().top, previousHeight);
-        previousHeight = $(window).scrollTop()
-        document.getElementById('title').style.transition = "height 0.3s ease";
-        document.getElementById('title').style.position = "fixed";
-        document.getElementById('title').style.top = "0";
-        document.getElementById('title').style.left = "40%" ;
-        document.getElementById('title').style.height = "7dvh";
-        document.getElementById('title').style.paddingBottom = "0";
-        document.getElementById('title').style.paddingTop = "0";
-        document.getElementById('title').onclick = onClickLink;
-        document.getElementById('title').style.cursor = "pointer";
-        document.getElementById('title').style.width = "20dvw";
-      }
-  }
-  else if (title.style.position == "fixed" && ($(window).scrollTop() < previousHeight)&& false){
-      if ($(window).scrollTop() <= window.innerHeight / 2 ) {
-        console.log($(title).offset().top, previousHeight);
-        previousHeight = $(window).scrollTop()
-        document.getElementById('title').style.transition = "0.3s ease";
-        document.getElementById('title').style.height = "20dvh";
-        document.getElementById('title').style.top = "30dvh";
-        document.getElementById('title').style.paddingBottom = "5dvh";
-        document.getElementById('title').style.paddingTop = "5dvh";
-        document.getElementById('title').style.position = "absolute";
-        document.getElementById('title').onclick = "";
-        document.getElementById('title').style.cursor = "default";
-        document.getElementById('title').style.width = "60%";
-        document.getElementById('title').style.left = "20%" ;
-      }
-    }
 });
