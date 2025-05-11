34016bed1fea1a1f8124ed286fac297ea057cf6e
Iain Visa <59170929+Thecnopapa@users.noreply.github.com>
2025-05-11 14:44:02 +0200

    Update main.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -20,7 +20,7 @@ $(window).scroll(function () {
   if (scroll >= maxScroll){
     let scroll = maxScroll
   }
-  title.style.height = String(7+(13*scroll/maxScroll)).concat("dvh")
+  title.style.height = String(20-(13*scroll/maxScroll)).concat("dvh")
 
 
   if (title.style.position == "absolute" && ($(window).scrollTop() >= previousHeight ) && false) {
