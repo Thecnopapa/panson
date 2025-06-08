7eea5e9e6bffdc2c8798fe9055d23aa73fc734ab
iainv <iainvisa@gmail.com>
2025-06-08 11:51:17 +0200

    Addd and delete fotos

--- firebase.py
+++ firebase.py
@@ -24,20 +24,23 @@ from firestore import load_files, upload_images
 
 def update_firebase(id, producte):
     sprint("Updating product: {}".format(id))
-    print(request.form)
-    paths = load_files()
+    #print(request.form)
+    paths = load_files(target_folder="productes")
     uploaded_images = upload_images(paths, "productes")
     data = {}
-    data["imatges"] = producte.imatges + uploaded_images
+    data["imatges"] = []
     for key, value in request.form.items():
-
-        if ":" in key:
+        if key == "img_list":
+            if value != "":
+                data["imatges"].append(value)
+        elif ":" in key:
             data_type = key.split(":")[0]
             db_key = key.split(":")[1]
             if data_type == "text":
                 data[db_key] = value
             if data_type == "bool":
                 data[db_key] = True if value in ["true", "on", True ,"1", 1, "True"] else False
+    data["imatges"] = data["imatges"] + uploaded_images
     print(data)
     productes.document(id).set(data, merge=True)
 
