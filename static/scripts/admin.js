70bde7c6dcece2358fd01d3ec4a77234ba20bdf4
iainv <iainvisa@gmail.com>
2025-07-14 00:00:51 +0200

    add items to lists

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -1,12 +1,38 @@
 
 
 
-function addMaterial(){
-    const materialList = document.getElementById('material_list');
-    const newMaterial = document.createElement("li");
-    const listLength = materialList.children.length -1;
-    console.log(materialList.children);
-    console.log(materialList.children.length);
-    newMaterial.innerHTML = '<span><input type="text" name="text:material#' +listLength + '" id="material" value=""> Preu <input type="number" name="number:material#$' +listLength + ':preu" id="material" value=""></span>'
-    materialList.lastElementChild.before(newMaterial);
+function addMaterial(button){
+    const targetList = button.parentElement;
+    const newItem = document.createElement("li");
+    const listLength = targetList.children.length -1;
+    console.log(targetList.children);
+    console.log(targetList.children.length);
+    newItem.innerHTML = '<span><input type="text" name="text:material#' +listLength + '"  class="material" value=""> Preu <input type="number" name="number:material#$' +listLength + ':preu" id="material" value=""></span>'
+    console.log("LAST:", targetList.lastElementChild);
+    console.log("NEW:",newItem);
+    targetList.lastElementChild.before(newItem);
+}
+
+function addExtra(button){
+    const targetList = button.parentElement;
+    const newItem = document.createElement("span");
+    const listLength = targetList.children.length -1;
+    console.log(targetList.children);
+    console.log(targetList.children.length);
+    newItem.innerHTML = '<input type="text" name="text:extra_colors#' +listLength + '" class="extra_colors" value=" ">'
+    console.log("LAST:", targetList.lastElementChild);
+    console.log("NEW:",newItem);
+    targetList.lastElementChild.before(newItem);
+}
+
+function addExtra(button){
+    const targetList = button.parentElement;
+    const newItem = document.createElement("span");
+    const listLength = targetList.children.length -1;
+    console.log(targetList.children);
+    console.log(targetList.children.length);
+    newItem.innerHTML = '<input type="text" name="text:extra_colors#' +listLength + '" class="extra_colors" value=" ">'
+    console.log("LAST:", targetList.lastElementChild);
+    console.log("NEW:",newItem);
+    targetList.lastElementChild.before(newItem);
 }
