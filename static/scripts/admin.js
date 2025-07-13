426e9f501df2bda5065443eee5847e94f7474a43
iainv <iainvisa@gmail.com>
2025-07-14 00:59:27 +0200

    remove buttons

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -7,7 +7,7 @@ function addMaterial(button){
     const listLength = targetList.children.length -1;
     console.log(targetList.children);
     console.log(targetList.children.length);
-    newItem.innerHTML = '<span><input type="text" name="text:material#' +listLength + '"  class="material" value=""> Preu <input type="number" name="number:material#$' +listLength + ':preu" id="material" value=""></span>'
+    newItem.innerHTML = '<span><input type="text" name="text:material#' +listLength + '"  class="material" value=""> Preu <input type="number" name="number:material#$' +listLength + ':preu" id="material" value=""><button class="remove" type="button" onclick="this.parentElement.parentElement.remove()">x</button></span>'
     console.log("LAST:", targetList.lastElementChild);
     console.log("NEW:",newItem);
     targetList.lastElementChild.before(newItem);
@@ -19,20 +19,21 @@ function addExtra(button){
     const listLength = targetList.children.length -1;
     console.log(targetList.children);
     console.log(targetList.children.length);
-    newItem.innerHTML = '<input type="text" name="text:extra_colors#' +listLength + '" class="extra_colors" value=" ">'
+    newItem.innerHTML = '<input type="text" name="text:extra_colors#' +listLength + '" class="extra_colors" value=" "><button class="remove" type="button" onclick="this.parentElement.parentElement.remove()">x</button>'
     console.log("LAST:", targetList.lastElementChild);
     console.log("NEW:",newItem);
     targetList.lastElementChild.before(newItem);
 }
 
-function addExtra(button){
+function addColour(button){
     const targetList = button.parentElement;
-    const newItem = document.createElement("span");
+    const newItem = document.createElement("li");
     const listLength = targetList.children.length -1;
     console.log(targetList.children);
     console.log(targetList.children.length);
-    newItem.innerHTML = '<input type="text" name="text:extra_colors#' +listLength + '" class="extra_colors" value=" ">'
+    newItem.innerHTML = '<span><input type="text" name="text:color#' +listLength + '" id="color" value=""> Preu <input type="number" name="number:color#' +listLength + ':preu" id="material" value=""><button class="remove" type="button" onclick="this.parentElement.parentElement.remove()">x</button></span>'
     console.log("LAST:", targetList.lastElementChild);
     console.log("NEW:",newItem);
     targetList.lastElementChild.before(newItem);
 }
+
