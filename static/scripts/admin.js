4c64320f8c3bb73d33eb3154b692d639db87c655
iainv <iainvisa@gmail.com>
2025-07-14 12:41:39 +0200

    variations admin

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -39,12 +39,24 @@ function addColour(button){
 
 function addSize(button) {
     const targetList = button.parentElement;
-    const newItem = document.createElement("span");
+    const newItem = document.createElement("li");
     const listLength = targetList.children.length - 1;
     console.log(targetList.children);
     console.log(targetList.children.length);
-    newItem.innerHTML = '<input type="text" name="text:talla#' + listLength + '" class="talla" value=""><button class="remove" type="button" onclick="this.parentElement.remove()">x</button>';
+    newItem.innerHTML = '<span><input type="text" name="text:talla#' + listLength + '" class="talla" value=""><input type="number" name="text:talla#' + listLength + ':qty" class="talla_quantitat" value="{{ info }}"><button class="remove" type="button" onclick="this.parentElement.remove()">x</button></span>';
     console.log("LAST:", targetList.lastElementChild);
     console.log("NEW:", newItem);
+    targetList.lastElementChild.before(newItem);
+}
+
+function addVariation(button){
+    const targetList = button.parentElement;
+    const newItem = document.createElement("li");
+    const listLength = targetList.children.length -1;
+    console.log(targetList.children);
+    console.log(targetList.children.length);
+    newItem.innerHTML = '<span><span><input type="text" name="text:variacio#' + listLength + '" class="variacio" value=""> Preu <input type="number" name="number:variacio#' + listLength + ':preu" class="variacio_preu" value=""><button class="remove" type="button" onclick="this.parentElement.parentElement.remove()">x</button></span></span>'
+    console.log("LAST:", targetList.lastElementChild);
+    console.log("NEW:",newItem);
     targetList.lastElementChild.before(newItem);
 }
