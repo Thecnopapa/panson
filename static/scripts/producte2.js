b2c3cb539150e9c783834f5f6913ddda981b59e6
iainv <iainvisa@gmail.com>
2025-07-26 19:16:34 +0200

    misc

--- static/scripts/producte2.js
+++ static/scripts/producte2.js
@@ -5,9 +5,7 @@ let form = document.getElementById('form');
 
 
 
-
 updatePrice();
-
 
 
 function updatePrice(){
@@ -55,6 +53,7 @@ function showInfoDropdown(trigger, popupContent, arrow=undefined) {
     }
 
 }
+
 function hideInfoDropdown(trigger, popupContent, arrow=undefined) {
     popupContent.style.display = "none";
     oldTrigger = String(trigger.attributes.onclick.value).split("(")[1];
@@ -64,13 +63,10 @@ function hideInfoDropdown(trigger, popupContent, arrow=undefined) {
     }
 }
 
-
-
 function showPopup(trigger, popupContent) {
     popupContent.style.display = "block";
     hideBackgound(popupContent);
 }
-
 
 function hidePopup(source, sourceElement) {
     let popupContent = undefined;
@@ -100,108 +96,102 @@ function addPopupCross(popupContent) {
     var cross = document.createElement("button");
     cross.className = "popup-cross";
     cross.innerHTML = "x";
-
     cross.type = "button";
     cross.setAttribute("onclick","hidePopup('cross', this)")
     popupContent.appendChild(cross);
 }
 
-
-
-
 function slideshowNext(slideshow){
-	console.log("Next");
 	if (slideshow === "producte"){
-    productSlideshow.update(1);
-	}
-}
-function slideshowPrev(slideshow){
-	console.log("Prev");
-	if (slideshow === "producte"){
-    productSlideshow.update(-1);
+        productSlideshow.update(1);
 	}
 }
 
+function slideshowPrev(slideshow){
+	if (slideshow === "producte"){
+        productSlideshow.update(-1);
+	}
+}
 
 class Slideshow{
-    constructor(container){
+    constructor(container) {
         this.imageContainer = container;
         console.log(this.imageContainer);
-	console.log(this.imageContainer.children);
+	    console.log(this.imageContainer.children);
         this.images = container.children;
         this.counter = 0;
         this.n_images = this.imageContainer.childElementCount;
         this.state = "still";
-	this.update(0);
+	    this.update(0);
     }
 
     selectImages(){
 	    console.log(this.counter + "/" + String(this.n_images-1));
-        if (this.counter === 0){
-		
-	    console.log("last img: " + this.imageContainer.lastElementChild);
+        if (this.counter === 0) {
             this.prevImg = this.imageContainer.lastElementChild;
-        }else{
+        } else {
             this.prevImg = this.images[this.counter - 1];
         }
         this.currentImg = this.images[this.counter];
-        if (this.counter === this.n_images-1){
+        if (this.counter === this.n_images-1) {
             this.nextImg = this.images[0];
-        }else{
+        } else {
             this.nextImg = this.images[this.counter + 1];
         }
-	console.log(this.prevImg);
-	console.log(this.currentImg);
-	console.log(this.nextImg);
-
+        console.log(this.prevImg);
+        console.log(this.currentImg);
+        console.log(this.nextImg);
     }
 
     update(change){
-        if (this.state === "still"){
-		console.log("Updating " + this.state);
+        if (this.state === "still") {
             this.state = "active";
-		console.log(this.counter +"+"+change);
             this.counter += Number(change);
-		console.log(this.counter);
-            if (this.counter >= this.n_images){
+            if (this.counter >= this.n_images) {
                 this.counter = 0;
             }
-            if (this.counter <0){
+            if (this.counter <0) {
                 this.counter = this.n_images-1;
             }
             this.selectImages();
             this.displayImages();
-        }else{console.log("slideshow active")}
+        } else {
+            console.log("slideshow active")
+        }
     }
 
 
-    displayImages(mode){
-	    console.log("displaying1");
-	    try{
-	for (let n = 0; n < this.n_images; n++) {
-		this.images[n].classList = "foto_producte"
-	}
-	this.currentImg.classList.add("current");
-	this.currentImg.classList.remove("previous");
-	this.currentImg.classList.remove("next");
-        this.prevImg.classList.add("previous");
-	this.prevImg.classList.remove("current");
-	this.nextImg.classList.add("next");
-	this.nextImg.classList.remove("current");
-	
-	    }catch(err){console.log(err.message);console.log(this.prevImg); console.log(this.currentImg);console.log(this.nextImg);};
+    displayImages(){
+	    try {
+            for (let n = 0; n < this.n_images; n++) {
+                this.images[n].classList = "foto_producte"
+            }
+            this.currentImg.classList.add("current");
+            this.currentImg.classList.remove("previous");
+            this.currentImg.classList.remove("next");
+            this.prevImg.classList.add("previous");
+            this.prevImg.classList.remove("current");
+            this.nextImg.classList.add("next");
+            this.nextImg.classList.remove("current");
+
+        } catch(err) {
+            console.log(err.message);
+            console.log(this.prevImg);
+            console.log(this.currentImg);
+            console.log(this.nextImg);
+        }
         setTimeout(this.displayImages2, 400, this);
     }
     displayImages2(t){
-	    console.log("displayong2");
-	console.log("state to still");
-	t.state = "still";
-	console.log(t.state);
+        t.state = "still";
     }
 }
-let productSlideshow = new Slideshow(document.getElementById("fotos-producte"));
 
-
+let productSlideshow = undefined;
+let fotos = document.getElementById("fotos-producte");
+if (fotos.children.length > 1){
+    productSlideshow = new Slideshow(fotos);
+}
 
 function displayBuyOptions(trigger){
 	optionDiv = document.getElementById("buy-options");
@@ -209,6 +199,7 @@ function displayBuyOptions(trigger){
 	optionDiv.scrollIntoView({"behaviour": "smooth"});
 	trigger.style.display ="none";
 }
+
 function hideBuyOptions(){
 	optionDiv = document.getElementById("buy-options");
 	buyButton = document.getElementById("buy");
