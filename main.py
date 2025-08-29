53e55d582d6d6177c30ce3aa5bdc6ece4a930b15
iainv <iainvisa@gmail.com>
2025-08-29 19:13:17 +0200

    error handling

--- main.py
+++ main.py
@@ -85,6 +85,22 @@ from app_essentials.html_builder import template
 from app_essentials.utils import get_opcions
 
 
+from werkzeug.exceptions import HTTPException
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return render_template("ERROR.html", code=e.code, name=e.name, description=e.description, request=request)
+
+
 
 
 
