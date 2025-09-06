a7ab54b7c1acb681fc2c2bf4a3b4cc093863f363
iain@lavander <iainvisa@gmail.com>
2025-09-06 11:47:35 +0000

    contact form macro

--- static/scripts/fetamida2.js
+++ static/scripts/fetamida2.js
@@ -3,16 +3,18 @@
 const toFirst = document.getElementById("to-first");
 const toSecond = document.getElementById("to-second");
 const toThird = document.getElementById("to-third");
-
+const sectionContainer = document.getElementById("sections-fetamida");
+let currentSection = 1;
 
 function toSection(targetSection) {
     if (targetSection === null) {
         targetSection = 1;
     }
+	if (targetSection > 2 || targetSection < 0){return;};
     window.history.replaceState(document.title, "", document.location.pathname+"?page=" + targetSection);
     const sectionWidth = window.innerWidth;
-    const sectionContainer = document.getElementById("sections-fetamida");
     print("TO: ", targetSection);
+	currentSection = targetSection;
     sectionContainer.scrollTo(sectionWidth * targetSection, 0 , {behavior: "smooth"});
     sectionContainer.style.maxHeight = String(sectionContainer.getElementsByClassName("fetamida-section")[targetSection].offsetHeight) + "px";
     if (targetSection === 0 ) {
@@ -30,6 +32,28 @@ function toSection(targetSection) {
         toThird.style.left = "calc(100% - " + String(toThird.offsetWidth) + "px)";
     }
 }
+
 window.addEventListener('load', function () {
     toSection(Number(new URL(document.URL).searchParams.get("page")));
 })
+
+let touchStart = 0;
+let touchEnd = 0;
+sectionContainer.addEventListener("touchstart", function(event){
+	touchStart = event.changedTouches[0].screenX;
+	print("TouchStart: ", touchStart);
+});
+sectionContainer.addEventListener("touchend", function(event){
+	touchEnd = event.changedTouches[0].screenX;
+	const touchX = touchEnd - touchStart;
+	print("TouchEnd: ", touchEnd, "Total: ", touchX, window.innerWidth);
+	if (touchX >= window.innerWidth*0.2){
+		toSection(currentSection - 1);
+	} else if (touchX*(-1) >= window.innerWidth*0.2){
+		toSection(currentSection + 1);
+	}
+});
+
+
+
+
