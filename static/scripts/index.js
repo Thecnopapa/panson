f03921d3d1f5be85710ac39e4847495f4d558502
iainv <iainvisa@gmail.com>
2025-09-23 19:32:14 +0200

    index style

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -19,29 +19,30 @@ function updateScroll() {
     } else {
         title.style.cursor = "default";
     }
-    title.style.top = String(35 - (35 * scroll / maxScroll)).concat("dvh");
-	//print(oscroll, scroll, maxScroll, title.style.top );
+
 
     if (window.innerHeight > window.innerWidth){
-        title.style.height = String(20 - (14 * scroll / maxScroll)).concat("dvh");
-        title.style.width = String(60 - (0 * scroll / maxScroll)).concat("dvw");
-        title.style.left = String(20 + (0 * scroll / maxScroll)).concat("dvw");
+        title.style.top = String(21.5 - (21.5 * scroll / maxScroll)).concat("vh");
+        title.style.height = String(20 - (14 * scroll / maxScroll)).concat("vh");
+        title.style.width = String(50 - (0 * scroll / maxScroll)).concat("vw");
+        title.style.left = String(25 + (0 * scroll / maxScroll)).concat("vw");
 	    }else {
+        title.style.top = String(35 - (35 * scroll / maxScroll)).concat("dvh");
         title.style.height = String(20 - (13 * scroll / maxScroll)).concat("dvh");
         title.style.width = String(60 - (40 * scroll / maxScroll)).concat("dvw");
         title.style.left = String(20 + (20 * scroll / maxScroll)).concat("dvw");
     }
 
 
-    title.style.paddingBottom = String(5 - (5 * scroll / maxScroll)).concat("dvh");
-    title.style.paddingTop = String(5 - (5 * scroll / maxScroll)).concat("dvh");
+    title.style.paddingBottom = String(5 - (5 * scroll / maxScroll)).concat("vh");
+    title.style.paddingTop = String(5 - (5 * scroll / maxScroll)).concat("vh");
     //print(oscroll, windowHeight*0.97);
     if (oscroll >= windowHeight*0.35) {
         title.style.zIndex = 60;
     } else {
         title.style.zIndex = 40;
     }
-    if (oscroll >= windowHeight*0.94) {
+    if ((oscroll >= windowHeight*0.94) && (window.innerHeight <= window.innerWidth) || (oscroll >= windowHeight*0.69) && (window.innerHeight > window.innerWidth)) {
         goBlack();
         title.classList.remove('white');
         //title.style.backgroundImage = "url(\"/static/media/logo-complet.png\")";
