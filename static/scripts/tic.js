56e60b4807625c8a4906e4e75f013255945f6383
iain@lavander <iainvisa@gmail.com>
2025-10-01 12:33:03 +0000

    c

---
+++ static/scripts/tic.js
@@ -0,0 +1,33 @@
+
+
+async function updateCookiesTic(container){
+	let inputAnalytic = container.getElementsByClassName("analytic")[0];
+	let inputEssential = container.getElementsByClassName("essential")[0];
+	let acceptedAnalytics = "denied";
+    	if (inputAnalytic.checked === true){
+        	acceptedAnalytics = "granted";
+        	consentAnalytics()
+    	}
+	console.log(inputAnalytic.checked, inputEssential.checked);
+	let r = await fetch("/accept-cookies", {
+		method:"POST",
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({ 
+			"cookies": {
+          			'ad_storage': 'denied',
+          			'ad_user_data': 'denied',
+          			'ad_personalization': 'denied',
+          			'analytics_storage': acceptedAnalytics,
+        		},
+			"essential": inputEssential.checked,
+		})
+	});
+	location.reload();
+
+}
+
+window.addEventListener("load", () => {
+	document.documentElement.scrollTo({top:0});
+})
