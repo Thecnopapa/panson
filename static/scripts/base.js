c277b71cdabe3a3514eec8ab6a931cbe98d27e8f
iain@lavander <iainvisa@gmail.com>
2025-10-03 11:47:35 +0000

    cookie preferences

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -60,6 +60,33 @@ async function acceptCookies(){
 
 
 
+async function updateCookiesTic(container){
+        let inputAnalytic = container.getElementsByClassName("analytic")[0];
+        let inputEssential = container.getElementsByClassName("essential")[0];
+        let acceptedAnalytics = "denied";
+            if (inputAnalytic.checked === true){
+                acceptedAnalytics = "granted";
+                consentAnalytics()
+            }
+        console.log(inputAnalytic.checked, inputEssential.checked);
+        let r = await fetch("/accept-cookies", {
+                method:"POST",
+                headers: {
+                        "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                        "cookies": {
+                                  'ad_storage': 'denied',
+                                  'ad_user_data': 'denied',
+                                  'ad_personalization': 'denied',
+                                  'analytics_storage': acceptedAnalytics,
+                        },
+                        "essential": inputEssential.checked,
+                })
+        });
+        location.reload();
+
+}
 
 
 
@@ -96,7 +123,7 @@ function hidePopup(source, sourceElement) {
         popupContent.remove()
     }
 
-    document.documentElement.style.overflow = "unset";
+    document.documentElement.style.overflow = "";
 }
 
 function hideBackgound(popupContent, cross=true) {
